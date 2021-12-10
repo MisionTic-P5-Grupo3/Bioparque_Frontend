11cc5af42b305ad277b9ad75991436caa9f9c7e1
Maria Gonzalez <mayigd0502@gmail.com>
2021-12-10 15:53:39 -0500

    Commit inicial

---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="login">
+    <h3>Iniciar Sesion</h3>
+    <form class="login_container">
+      <div>
+        <label>Correo Electrónico</label>
+        <input
+          v-model.number="mail"
+          type="mail"
+          placeholder="Escribe tu correo electrónico"
+        />
+      </div>
+      <div>
+        <label>Contraseña</label>
+        <input
+          v-model.number="password"
+          type="password"
+          placeholder="Escribe tu contraseña"
+        />
+      </div>
+      <button type="submit" class="btn btn-login">Iniciar Sesion</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+#login h3 {
+  font-size: 35px;
+}
+.login_container {
+  border: 3px solid black;
+  height: 300px;
+  width: 500px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.login_container div {
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+  margin: 0 auto;
+  height: 97px;
+}
+.login_container div label {
+  font-size: 18px;
+  text-align: start;
+  margin: 30px 0 10px;
+}
+.login_container div input {
+  height: 200px;
+}
+.btn-login {
+  margin-top: 20px;
+}
+</style>
